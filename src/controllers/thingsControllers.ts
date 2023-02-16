4d90c73e91dbb0b1ff0ee87003595c4d7b3bdd73
Gaston82 <cabezaartaza@hotmail.com>
2023-02-16 22:21:34 +0100

    add getThing by id method

--- src/controllers/thingsControllers.ts
+++ src/controllers/thingsControllers.ts
@@ -4,3 +4,9 @@ import { type Request, type Response } from "express";
 export const getThings = (req: Request, res: Response) => {
   res.status(200).json({ things });
 };
+
+export const getThingById = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const thing = things.find((thing) => thing.id === +id);
+  res.status(200).json({ thing });
+};
--- src/routers/thingsRouters.ts
+++ src/routers/thingsRouters.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getThings } from "../controllers/thingsControllers.js";
+import { getThingById, getThings } from "../controllers/thingsControllers.js";
 
 // eslint-disable-next-line new-cap
 const thingsRouters = Router();
 
 thingsRouters.get("/", getThings);
+thingsRouters.get("/:id", getThingById);
 
 export default thingsRouters;
